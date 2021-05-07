f3d4b50bc1c4b091d922991b48d52be21e591d35
DeliciousLlama <248266816j@gmail.com>
2021-05-07 07:21:52 -0700

    added todo

--- src/com/source/excEnv/main/GameMain.java
+++ src/com/source/excEnv/main/GameMain.java
@@ -18,10 +18,10 @@ public class GameMain {
 		//inital frame
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		frame = new JFrame(titleName);	//make new frame
-		GAME_WIDTH = ((int) tk.getScreenSize().getWidth()/2);
-		GAME_HEIGHT = ((int) tk.getScreenSize().getHeight()/2);
-//		GAME_WIDTH = 500;
-//		GAME_HEIGHT = 500;
+//		GAME_WIDTH = ((int) tk.getScreenSize().getWidth()/2);
+//		GAME_HEIGHT = ((int) tk.getScreenSize().getHeight()/2);
+		GAME_WIDTH = 1920/2;
+		GAME_HEIGHT = 1080/2;
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//colse operation
 		frame.setResizable(false);	//non-resizable
 //		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
